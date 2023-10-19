5e344e2db8fb6c3a83e2b07d4a5b3a32b225dba3
Juan Ramirez <jmramirez1497@gmail.com>
2023-10-19 17:23:19 -0500

    Firts Commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({category}) => {
+
+    const { images, isLoading } = useFetchGifs(category);
+
+    console.log({images,isLoading})
+    
+    return (
+        <>
+            <h3>{ category }</h3>
+            {
+                isLoading && (<h2>Cargando...</h2>)
+            }
+            <ol className="card-grid">
+                { 
+                    images.map( (image) => {
+                    //    return <li key={id}>{title}</li>
+                       return <GifItem key={image.id} {...image}/>
+                    })
+                }
+            </ol>
+        </>
+    )
+}
